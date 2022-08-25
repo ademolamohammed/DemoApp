3ab3b5b55c6ad2f32441dc2fe82b39ba81cb83d1
ademolamohammed <hammedmoh06@gmail.com>
2022-08-25 21:43:30 +0100

    made lil fixes

--- src/components/header/headerTab.tsx
+++ src/components/header/headerTab.tsx
@@ -29,7 +29,7 @@ const HeaderTab = () => {
                     <button  onClick={() =>toast.success("Buy template clicked")} className="btn mb-3 radius btn-outline-secondary text-dark mr-4 px-4 font-weight-bold template">
                         Buy template
                     </button>
-                    <button  onClick={() =>toast.success("Explore more clicked")}   className="btn mb-3 border btn-outline-secondary radius text-white border-white px-5 font-weight-bold"> 
+                    <button  onClick={() =>toast.success("Explore more clicked")}  className="btn mb-3 border btn-outline-secondary radius text-white border-white px-5 font-weight-bold"> 
                         Explore
                     </button>
                 </div>
